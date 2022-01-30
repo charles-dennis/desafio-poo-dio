53faea27228ea3faa1bd1cefc28d93d84f9ac059
Charles Dennis <charlesdennis2009@hotmail.com>
2022-01-30 14:13:42 -0300

    ligando as classes de dominio

--- src/br/com/dio/desafio/dominio/CursoAceleracao.java
+++ src/br/com/dio/desafio/dominio/CursoAceleracao.java
@@ -1,5 +1,27 @@
 package br.com.dio.desafio.dominio;
 
-public class CursoAceleracao {
+import java.util.HashSet;
+import java.util.Set;
 
+public class CursoAceleracao extends Curso{
+	
+	private Set<Dev> devsInscritos = new HashSet<>();
+	private Conteudo conteudo;
+
+	public Conteudo getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(Conteudo conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public Set<Dev> getDevsInscritos() {
+		return devsInscritos;
+	}
+
+	public void setDevsInscritos(Set<Dev> devsInscritos) {
+		this.devsInscritos = devsInscritos;
+	}
+	
 }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -1,5 +1,31 @@
 package br.com.dio.desafio.dominio;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
 
+    private String data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    public Mentoria() {
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String string) {
+        this.data = string;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
 }
